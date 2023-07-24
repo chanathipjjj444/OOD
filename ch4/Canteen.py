aa59ce2008b331a0189ded3ba58234e10b8ed041
Jay <chanathip2320@gmail.com>
2023-07-25 00:13:29 +0700

    search portal

--- ch4/Canteen.py
+++ ch4/Canteen.py
@@ -1,25 +1,82 @@
-class Cashier:
-    def __init__(self, real):
-        self.cashier1 = []
-        self.cashier2 = []
-        self.items = real
-
-        self.sec = 0
-
-    def enQueue(self, i):
-        self.items.append(i)
-
-    def deQueue(self, i):
-        return i.pop(0)
+class Queue():
+    def __init__(self, items=None, max=None, ) -> None:
+        if items is None:
+            self.items = []
+        else:
+            self.items = items
+        
+        self.max = max
     
-    def isEmty(self):
-        return self.items == []
+    def __str__(self) -> str:
+        return str(self.items)
 
-    def size(self):
+    def __len__(self) -> int:
         return len(self.items)
 
+    def __getitem__(self, i:int):
+        return self.items[i]
+
+    def isEmpty(self):
+        return len(self) == 0
+
+    def isFull(self):
+        if self.max is not None:
+            return len(self) == self.max
+        else:
+            return False
+
+    def deQueue(self):
+        if not self.isEmpty():
+            return self.items.pop(0)
+
+    def enQueue(self, x):
+        if not self.isFull():
+            self.items.append(x)
+
+class Canteen(Queue):
+    def __init__(self, id:list, queue:list):
+        super().__init__()
+        self.id = id
+        self.queue = queue
+    
+    def checkIndex(self, x):
+        if self.isEmpty():
+            return 0
+        idx = 0
+        c = 0
+        for index, e in enumerate(self):
+            if e[0] == x[0]:
+                if index > idx:
+                    idx = index
+                c += 1
+        if idx == 0 and c == 0:
+            return len(self)
+        return idx + 1
+
+    def check(self, x):     
+        if self.isEmpty():
+            self.enQueue(x)
+        else:
+            idx = self.checkIndex(x)
+            self.items.insert(idx, x)
+                
+            
+    def solve(self):
+        for index, q in enumerate(self.queue):
+            if len(q) > 1:
+                q = q.split()
+                self.check(q[1])
+            else:
+                if self.isEmpty():
+                    print('Empty')
+                else:
+                    print(self.deQueue())
+        
 
 
+inp = input('Enter Input : ').split('/')
 
-
-a = input("Enter Input : ").split(",")
+employee = inp[0].split(',')
+queue = inp[1].split(',')
+canteen = Canteen(employee, queue)
+canteen.solve()
