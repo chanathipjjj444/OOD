1a201aaf840e407dda076fa6b7a3060421ec884c
Jay <chanathip2320@gmail.com>
2023-08-12 11:30:09 +0700

    fixed perket

--- ch6/perket.py
+++ ch6/perket.py
@@ -1,34 +1,33 @@
-def power(base, top, ans=1):
-    if top > 0:
-        ans = ans * base
-        power(base, top - 1, ans)
-    if top == 0:
-        print(ans)
+def diff(sour:int, bitter:int):
+    return abs(sour - bitter)
 
-
-def sour(sour_l: list):
-    if len(sour_l) == 0:
-        return 1
-    return sour_l[0] * sour(sour_l[1:])
-
-
-def bitter(bitter_l: list):
-    if len(bitter_l) == 0:
-        return 0
+def find(length, sour, bitter):
+    global best
+    if length == length_inp:
+        if bitter > 0 and diff(sour, bitter) < best:
+            best = diff(sour, bitter)
     else:
-        return bitter_l[0] + bitter(bitter_l[1:])
+        find(length + 1,sour, bitter)
+        find(length + 1, sour * sour_l[length], bitter + bitter_l[length])
+        
+
+
 
 
 inp = input("Enter Input : ").split(",")
+
+length_inp= len(inp)
+
 sour_l = []
 bitter_l = []
+best = 9999
+
 
 for i in inp:
     i = i.split(" ")
     sour_l.append(int(i[0]))
     bitter_l.append(int(i[1]))
 
+find(0, 1, 0)
 
-print(sour(sour_l))
-print(bitter(bitter_l))
-print(abs(sour(sour_l) - bitter(bitter_l)))
+print(best)
